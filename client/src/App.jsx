68703da061b0f975f04fefbe955f775646d6f949
kathu@08 <sivalingamk.19@uom.lk>
2021-10-12 21:38:16 +0530

    corrected errors

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import { Route, Redirect } from "react-router-dom";
+
+// HOC
+import HomeLayoutHOC from "./HOC/Home.Hoc";
+import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
+
+import Temp from "./Components/temp";
+
+// pages
+import Home from "./Page/Home";
+import Overview from "./Page/Restaurant/Overview";
+import OrderOnline from "./Page/Restaurant/OrderOnline";
+
+function App() {
+  return (
+    <>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <Route path="/restaurant/:id" exact>
+        <Redirect to="/restaurant/:id/overview" />
+      </Route>
+      <HomeLayoutHOC path="/:type" exact component={Home} />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/overview"
+        exact
+        component={Overview}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/order-online"
+        exact
+        component={OrderOnline}
+      />
+      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/reviews"
+        exact
+        component={Temp}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/photos"
+        exact
+        component={Temp}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,61 @@
+// Importing Env variables
+require("dotenv").config();
+
+// Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+
+// config
+import googleAuthConfig from "./config/google.config";
+import routeConfig from "./config/route.config";
+
+// microservices routes
+import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+import Food from "./API/Food";
+import Menu from "./API/menu";
+import Image from "./API/Image";
+import Order from "./API/orders";
+import Reviews from "./API/reviews";
+import User from "./API/User";
+import Menu from "./API/menu";
+
+// Database connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+// application middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended: false }));
+zomato.use(helmet());
+zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// passport configuration
+googleAuthConfig(passport);
+routeConfig(passport);
+
+// Application Routes
+zomato.use("/auth", Auth);
+zomato.use("/restaurant", Restaurant);
+zomato.use("/food", Food);
+zomato.use("/menu", Menu);
+zomato.use("/food", Image);
+zomato.use("/order", Order);
+zomato.use("/reviews", Reviews);
+zomato.use("/user", User);
+zomato.use("/menu", Menu);
+
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
+
+zomato.listen(4000, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running"))
+    .catch(() =>
+      console.log("Server is running but database connection failed ...")
+    )
+);
